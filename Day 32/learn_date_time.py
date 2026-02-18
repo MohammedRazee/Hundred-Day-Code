21b7db83f2b54bbf1f2fe8a33660ab1fd099c849
Mohammed Razee <razeeismail15@gmail.com>
2026-02-18 10:34:31 +0530

    Changes

--- Day 32/learn_date_time.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.day
-
-date_of_birth = dt.datetime(year=2002, month=2, day=24, hour=19, minute=30)
-print(date_of_birth)
